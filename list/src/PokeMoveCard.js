a1d1882ce24b293aa7b818feabc23018f34b8ee4
Sujal Shrestha <sujal.scxi121@amv.edu.np>
2023-08-16 14:47:13 +0545

    completed list

---
+++ list/src/PokeMoveCard.js
@@ -0,0 +1,11 @@
+import "./PokeMoveCard.css";
+export default function PokeMoveCard({ id, type, move, level }) {
+  return (
+    <li className="poke-move-card">
+      <h3>Move {id}</h3>
+      <h4 style={{ padding: 10 }}>{move.toUpperCase()}</h4>
+      <p>Type:{type}</p>
+      <p>Level: {level}</p>
+    </li>
+  );
+}
