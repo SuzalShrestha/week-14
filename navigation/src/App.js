338e4f946ccab2ffaab62a107e0e2057189fb8fc
Sujal Shrestha <sujal.scxi121@amv.edu.np>
2023-08-24 13:58:48 +0545

    completed navigation

--- navigation/src/App.js
+++ navigation/src/App.js
@@ -1,20 +1,78 @@
-import { Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
-import Stocks from './components/Stocks';
-import Movies from './components/Movies';
+import { Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import Stocks from "./components/Stocks";
+import Movies from "./components/Movies";
+import { Link, NavLink, useHistory } from "react-router-dom";
 
+const handleClick = () => {
+  const history = useHistory();
+  window.alert("Sending info to the db");
+  history.push("/");
+};
 function App() {
   return (
-    <div className='main'>
+    <div className="main">
       <h1>App Component</h1>
+      <button onClick={handleClick}>Home</button>
+      <h2>Link tag</h2>
+      <nav className="comp nav">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/stocks">Stocks</Link>
+          </li>
+          <li>
+            <Link to="/movies">Movies</Link>
+          </li>
+        </ul>
+      </nav>
+      <h2>NavLink Tag</h2>
+      <nav className="comp nav">
+        <ul>
+          <li>
+            <NavLink
+              exact
+              activeStyle={{ fontWeight: "bold" }}
+              activeClassName="purple"
+              to="/"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            {" "}
+            <NavLink
+              exact
+              activeStyle={{ fontWeight: "bold" }}
+              activeClassName="purple"
+              to="/stocks"
+            >
+              Stocks
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              exact
+              activeStyle={{ fontWeight: "bold" }}
+              activeClassName="purple"
+              to="/movies"
+            >
+              Movies
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
       <Switch>
-        <Route exact path='/'>
+        <Route exact path="/">
           <Home />
         </Route>
-        <Route path='/stocks'>
+        <Route path="/stocks">
           <Stocks />
         </Route>
-        <Route path='/movies'>
+        <Route path="/movies">
           <Movies />
         </Route>
         <Route>
--- navigation/src/components/Stocks/index.js
+++ navigation/src/components/Stocks/index.js
@@ -1,6 +1,11 @@
+import { Redirect } from "react-router-dom";
+let loggedIn = true;
 function Stocks() {
+  if (!loggedIn) {
+    return <Redirect to="/not-logged-in" />;
+  }
   return (
-    <div className='comp orange'>
+    <div className="comp orange">
       <h1>Stocks Component</h1>
     </div>
   );
